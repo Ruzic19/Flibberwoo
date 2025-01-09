f1531a9c7cc42cbf5723391ea7ed6903e917f2e6
Itzo <ja.sam.ico@gmail.com>
2025-01-09 23:32:55 +0100

    Added GameMenu Scene

--- src/main.js
+++ src/main.js
@@ -1,29 +1,36 @@
 import Phaser from "phaser";
+import GameMenu from "./scene/GameMenu"
 
 const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: {
-        preload: preload,
-        create: create,
-    },
+    parent: 'game',
+    backgroundColor: '#000000',
+    scene: [GameMenu],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    }
 };
 
-const game = new Phaser.Game(config);
+new Phaser.Game(config);
 
-function preload() {
-    this.load.image('logo', );
-}
+// function preload() {
+//     this.load.image('logo', );
+// }
 
-function create() {
-    const logo = this.add.image(400, 300, 'logo');
-    this.tweens.add({
-        targets: logo,
-        y: 850,
-        duration: 2000,
-        ease: 'Power2',
-        yoyo: true,
-        loop: -1,
-    });
-}
+// function create() {
+//     const logo = this.add.image(400, 300, 'logo');
+//     this.tweens.add({
+//         targets: logo,
+//         y: 850,
+//         duration: 2000,
+//         ease: 'Power2',
+//         yoyo: true,
+//         loop: -1,
+//     });
+//}
---
+++ src/scenes/GameMenu.js
@@ -0,0 +1,35 @@
+export default class GameMenu extends Phaser.Scene {
+    constructor() {
+        super({ key: 'GameMenu' });
+    }
+
+    create() {
+        // Get the center coordinates of the game
+        const centerX = this.cameras.main.centerX;
+        const centerY = this.cameras.main.centerY;
+
+        // Add title text
+        this.add.text(centerX, centerY - 100, 'Endless Runner', {
+            fontSize: '48px',
+            fill: '#fff'
+        }).setOrigin(0.5);
+
+        // Create start button
+        const startButton = this.add.text(centerX, centerY, 'Start Game', {
+            fontSize: '32px',
+            fill: '#fff',
+            backgroundColor: '#4a4a4a',
+            padding: { x: 20, y: 10 }
+        })
+        .setOrigin(0.5)
+        .setInteractive()
+        .on('pointerover', () => startButton.setStyle({ fill: '#ff0' }))
+        .on('pointerout', () => startButton.setStyle({ fill: '#fff' }))
+        .on('pointerdown', () => {
+            // This will be used later to start the game
+            console.log('Game Start clicked!');
+            // You can add scene transition here:
+            // this.scene.start('GameScene');
+        });
+    }
+}
